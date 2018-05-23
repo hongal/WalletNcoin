8d8ae98fcde24f9aef9a0d689c6c50a155dd692e
hongal <qnpfrajtls@gmail.com>
2018-05-23 13:27:37 +0900

    spring boot 기본설정

---
+++ src/main/java/com/geopia/wallet_ncoin/Test/TestController.java
@@ -0,0 +1,22 @@
+package com.geopia.wallet_ncoin.Test;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class TestController {
+    @Value("${spring.application.name}")
+    String appName;
+
+    @GetMapping("/")
+    public String home(Model model){
+        System.out.print(appName);
+        model.addAttribute("appName", appName);
+
+        return "home";
+    }
+}
---
+++ src/main/java/com/geopia/wallet_ncoin/WalletNcoinApplication.java
@@ -0,0 +1,12 @@
+package com.geopia.wallet_ncoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletNcoinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WalletNcoinApplication.class, args);
+    }
+}
