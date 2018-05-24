d586b76b219490e6ab4e5bb9b14aa9250e99ec1f
whiw <youngou330@gmail.com>
2018-05-24 15:53:05 +0900

    minor change

--- src/main/java/com/geopia/wallet_ncoin/controller/TransListController.java
+++ src/main/java/com/geopia/wallet_ncoin/controller/TransListController.java
@@ -1,4 +1,4 @@
-package com.geopia.wallet_ncoin.Controller;
+package com.geopia.wallet_ncoin.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
