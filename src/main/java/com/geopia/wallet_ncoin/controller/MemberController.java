22fa87712f9b2711c3f917e3592f1ff498bab936
whiw <youngou330@gmail.com>
2018-05-28 09:20:51 +0900

    라이브러리 추가

--- src/main/java/com/geopia/wallet_ncoin/controller/MemberController.java
+++ src/main/java/com/geopia/wallet_ncoin/controller/MemberController.java
@@ -10,4 +10,6 @@ public class MemberController {
 
         return "tiles/default/modify";
     }
+    
+  
 }
--- src/main/java/com/geopia/wallet_ncoin/controller/RegisterController.java
+++ src/main/java/com/geopia/wallet_ncoin/controller/RegisterController.java
@@ -1,13 +1,47 @@
-package com.geopia.wallet_ncoin.Controller;
+package com.geopia.wallet_ncoin.controller;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.geopia.wallet_ncoin.dto.NcoinCustomerDto;
+import com.geopia.wallet_ncoin.mapper.NcoinCustomerMapper;
 
 @Controller
 public class RegisterController {
+	
+	@Autowired
+	NcoinCustomerMapper ncoincustomermapper;
+	
     @RequestMapping("/register")
     public String getRegisterPage(){
 
         return "tiles/none/register";
     }
+    
+    @RequestMapping("signin")
+    public String getSignin() {
+    	
+    	return "tiles/none/signin";
+    }
+    
+
+	@RequestMapping("/join")
+	public String join(Map<String, Object> param,ModelMap  mv,NcoinCustomerDto customerdto) {
+		
+		try {
+			ncoincustomermapper.addcustomer(customerdto);
+			mv.addAttribute("isSuccess", "true");
+		} catch (Exception e) {
+			
+			
+			mv.addAttribute("isSuccess", "false");
+			mv.addAttribute("errMsg", e.getMessage());
+		}
+		
+		return "tiles/none/login";
+	}
 }
